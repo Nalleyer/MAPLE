69a80cda0891f79348b07b0777a6f49559da772f
nalleyer <nalleyer@gmail.com>
2020-08-02 18:11:12 +0800

    running lua to show ui

--- src/lua.rs
+++ src/lua.rs
@@ -1,4 +1,4 @@
-use rlua::{Function, Lua, MetaMethod, Result, UserData, UserDataMethods, Variadic};
+use rlua::{Function, Lua, MetaMethod, Result, UserData, UserDataMethods, Variadic, Table};
 
 use imgui::*;
 
@@ -10,14 +10,23 @@ pub struct MpLua {
 
 impl MpLua {
     pub fn new(file_content: &str) -> Self {
-        MpLua {
-            lua: Lua::new()
-        }
+        let lua = Lua::new();
+        lua.context(|lua_ctx| {
+            lua_ctx.load(&file_content).exec().expect("loading lua files");
+        });
+        MpLua { lua }
     }
 
     pub fn make_render<'ui>(&self, ui: &'ui imgui::Ui) -> Box<dyn FnOnce() -> () + 'ui> {
+        let mut ui_str = String::from("");
+        self.lua.context(|lua_ctx| {
+            let globals = lua_ctx.globals();
+            let func_update = globals.get::<_, Function>("update").expect("gettting lua entry");
+            func_update.call::<_, ()>(0).expect("calling update");
+            ui_str = globals.get::<_, Table>("mp_state").expect("getting mp_state").get("num").unwrap();
+        });
         Box::new(move || {
-            ui.text(im_str!("i love lua"));
+            ui.text(ui_str);
         })
     }
 }
