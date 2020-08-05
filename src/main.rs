2e0dbf64fcca5b345391fa83c45f679eabbae654
Nalleyer <nalleyer@gmail.com>
2020-08-05 23:10:50 +0800

    add new subcommand

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@ use clap::{App, Arg, SubCommand};
 mod imgui_wrapper;
 mod lua;
 mod run;
+mod new;
 
 use crate::run::run;
+use crate::new::new;
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let matches = App::new("maple")
@@ -18,12 +20,25 @@ fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
                 .author("nalleyer")
                 .arg(Arg::with_name("INPUT").required(true)),
         )
+        .subcommand(
+            SubCommand::with_name("new")
+                .about("generate lua scaffold")
+                .version("0.1.0")
+                .author("nalleyer")
+                .arg(Arg::with_name("FILENAME").required(true)),
+        )
         .get_matches();
 
     if let Some(matches) = matches.subcommand_matches("run") {
         let input_path = matches.value_of("INPUT").unwrap();
         run(&input_path)?;
     }
+
+    if let Some(matches) = matches.subcommand_matches("new") {
+        let file_name = matches.value_of("FILENAME").unwrap();
+        new(&file_name)?;
+    }
+
 
     Ok(())
 }
